4669255fece9f0dffbf69df1d637d6960a4d0cd3
fabian247 <onlinezeug@gmx.net>
2017-02-26 16:10:56 +0100

    Added more basic tests

--- test/expose_websites-test.js
+++ test/expose_websites-test.js
@@ -5,11 +5,23 @@ const filename = 'iad-if-wlan_24.02.17_2029.eth'
 
 buster.testCase('get Decoded Packets', {
   'should return decoded packets': function () {
-    var result = app.getDecodedPackets(filename)
-    return result
-      .then(() => {
-        console.log(result)
-        buster.assert.equals(5375, result.packets.length)
-      })
+    return app.getDecodedPackets(filename)
+    .then((res) => {
+      console.log(res.packets.length)
+      buster.assert.equals(5375, res.packets.length)
+    })
+  }
+})
+
+buster.testCase('get Websites', {
+  setUp: function () {
+    this.decPackets = app.getDecodedPackets(filename)
+  },
+  'should return Websites': function () {
+    return app.getWebsites(this.decPackets)
+    .then((res) => {
+      console.log(res.packets.length)
+      buster.assert.equals(5375, res.packets.length)
+    })
   }
 })
