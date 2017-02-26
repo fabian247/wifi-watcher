28ae0e96e2b4d19bff08498ca0807a6cc7477952
fabian247 <onlinezeug@gmx.net>
2017-02-26 15:56:43 +0100

    Added promises to wait for end of file

--- test/expose_websites-test.js
+++ test/expose_websites-test.js
@@ -1,10 +1,15 @@
 const buster = require('buster')
-// const app = require('../lib/expose_websites')
+const app = require('../lib/expose_websites')
 
 const filename = 'iad-if-wlan_24.02.17_2029.eth'
 
 buster.testCase('get Decoded Packets', {
   'should return decoded packets': function () {
-    buster.assert.match(filename, filename)
+    var result = app.getDecodedPackets(filename)
+    return result
+      .then(() => {
+        console.log(result)
+        buster.assert.equals(5375, result.packets.length)
+      })
   }
 })
