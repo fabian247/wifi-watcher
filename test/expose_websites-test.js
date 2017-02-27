c248c44a3b064ab5defead5294ac081a9d00ef97
fabian247 <onlinezeug@gmx.net>
2017-02-27 20:19:35 +0100

    Added more tests

--- test/expose_websites-test.js
+++ test/expose_websites-test.js
@@ -7,13 +7,22 @@ buster.testCase('get Decoded Packets', {
   'should return decoded packets': function () {
     return app.getDecodedPackets(filename)
     .then((res, err) => {
-      console.log('Test: decoded %d packages.', res.packets.length)
       buster.assert.equals(5375, res.packets.length)
     })
   },
-  'should reject promise if no file or empty file': function () {
-    buster.assert.match(app.getDecodedPackets('')
-    .then(() => {}), Promise.reject(new Error('Could not open file.')))
+  'should reject promise if no file': function () {
+    return app.getDecodedPackets('')
+    .then((res) => {}, (err) => {
+      buster.assert.equals(err, new Error())
+      buster.assert.match(err.message, 'No such file')
+    })
+  },
+  'should reject promise if file is empty': function () {
+    return app.getDecodedPackets('emptyFile.txt')
+    .then((res) => {}, (err) => {
+      buster.assert.equals(err, new Error())
+      buster.assert.match(err.message, 'File is empty')
+    })
   }
 })
 
@@ -29,8 +38,6 @@ buster.testCase('get Websites', {
     })
     .then((res) => {
       var promisedWebsites = app.getWebsites(result)
-      console.log('Test Websites: ')
-      console.log('dhosts: %d shosts: %d', promisedWebsites.dhosts.length, promisedWebsites.shosts.length)
       buster.assert.equals(5375, promisedWebsites.dhosts.length)
       buster.assert.equals(5375, promisedWebsites.shosts.length)
     })
