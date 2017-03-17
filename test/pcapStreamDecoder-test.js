2780df4510bdd674468fb11f2bb1abf8bd6d6e43
fabian247 <onlinezeug@gmx.net>
2017-03-17 18:55:07 +0100

    Packet processing in seperate process, cleanup

---
+++ test/pcapStreamDecoder-test.js
@@ -0,0 +1,14 @@
+const buster = require('buster')
+const pcap = require('pcap')
+const app = require('../lib/pcapStreamDecoder')
+
+buster.testCase('createPcapSessionFromStdin', {
+  'should return pcapSession': function () {
+    this.stub(pcap, 'createOfflineSession')
+    app.createPcapSessionFromStdin()
+    buster.assert.calledOnce(pcap.createOfflineSession)
+  },
+  'should throw error if creation of pcapSession fails': function () {
+    this.stub(app, 'createPcapSessionFromStream')
+  }
+})
